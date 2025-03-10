3b601866b5da7bbd67f2896b00a282490efa8ef9
Andrew Batallones <batallonesa@gmail.com>
2025-03-10 10:50:05 -0400

    Fix Javadocs formatting

--- src/leetcode/Utils.java
+++ src/leetcode/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
     /**
      * Prints out an array of objects.
      * Takes in an array of any object T and prints it.
-    */
+     */
     public static <T> void printArray(T[] array) {
         for (T element : array) {
             System.out.print(element + " ");
